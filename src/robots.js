77975f8cfd0e89481cc4d24e946f32cb681ec659
Mubarak Showole <mubarakshow@github.com>
2019-02-11 06:48:43 +0100

    added a new card component that loads dynamic images from RoboHash APIs

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1, 
+    name: 'Mubarak Show',
+    username: 'mubarakshow',
+    email: 'muabrak4show@yahoo.com' 
+  },
+  {
+    id: 2, 
+    name: 'Hikmat Show',
+    username: 'hikmatshow',
+    email: 'hikmatshow@gmail.com' 
+  },
+  {
+    id: 3, 
+    name: 'Teslim Show',
+    username: 'tezzyshow',
+    email: 'tezzyshow@hotmail.com' 
+  },
+  {
+    id: 4, 
+    name: 'Yusrah Show',
+    username: 'yusbaby',
+    email: 'yusrahshow@gmail.com' 
+  },
+]
